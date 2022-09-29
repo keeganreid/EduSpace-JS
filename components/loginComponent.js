160a8b91633f4bd7b4f88130de77998ebc740898
IsabelleHeyl <84284121+IsabelleHeyl@users.noreply.github.com>
2022-09-29 15:19:22 +0200

    Added Keegans stuff

---
+++ components/loginComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function loginComponent() {
+  return <div> login </div>;
+}
+
+export default loginComponent;
---
+++ pages/pages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './login';
+
+function pages() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default pages;
