7821d16fd684c69637992fc72b5c4e1fcbb3cc6d
IsabelleHeyl <84284121+IsabelleHeyl@users.noreply.github.com>
2022-09-30 12:39:35 +0200

    2 files modified, 3 added

---
+++ pages/Home.jsx
@@ -0,0 +1,13 @@
+import Dashboard from '../components/Dashboard';
+import Profile from '../components/Profile';
+
+function Home() {
+  return (
+    <div>
+      <Dashboard />
+      <Profile />
+    </div>
+  );
+}
+
+export default Home;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Pages from '../pages/pages';
-import ListUsers from '../components/listUsers';
-import LoginComponent from '../components/loginComponent';
+// import React from 'react';
+// import Pages from '../pages/pages';
+// import ListUsers from '../components/listUsers';
+// import LoginComponent from '../components/loginComponent';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <div>
+//       <ListUsers />
+//       <LoginComponent />
+//     </div>
+//   );
+// }
+
+import Home from '../pages/Home';
+
+function App() {
   return (
-    <div>
-      <ListUsers />
-      <LoginComponent />
+    <div className="App">
+      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
+
+export default App;
